c44d550b38ff05c45a208a516724b7fdec7758bf
Kulothungan16 <kulothungan@123>
2021-10-04 19:03:41 +0530

    Add 'noreferrer' to Link

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Link = ({ children, ...rest  }) => (
-  <A target='_blank' rel='noopener' {...rest}>
+  <A target='_blank' rel='noopener noreferrer' {...rest}>
     {children}
   </A>
 );
