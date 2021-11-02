2483174ed542c21d88228f945a996b2193991fae
Kulothungan16 <kulothunganug@gmail.com>
2021-11-02 12:26:28 +0530

    Some colors on Blogs page

--- src/pages/blogs/index.jsx
+++ src/pages/blogs/index.jsx
@@ -3,10 +3,16 @@ import styled from 'styled-components';
 
 const Blogs = () => (
   <Container>
-    <p>Welcome to Blogs!</p>
+    <P>Welcome to Blogs!</P>
   </Container>
 );
 
-const Container = styled.div``;
+const Container = styled.div`
+  color: ${p => p.theme.textColor};
+  background-color: ${p => p.theme.backgroundColor};
+  height: 100vh;
+`;
+
+const P = styled.p``;
 
 export default Blogs;
