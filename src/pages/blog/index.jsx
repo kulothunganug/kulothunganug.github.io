4298e7acd49078afcf27d306a5b2b9362e345ba0
Kulothungan16 <kulothunganug@gmail.com>
2021-11-04 19:15:30 +0530

    Updated blogs and blog/* page

--- src/pages/blog/index.jsx
+++ src/pages/blog/index.jsx
@@ -1,15 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { Redirect } from 'wouter';
+import { Redirect, useLocation } from 'wouter';
+import ReactMarkdown from 'react-markdown';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+import { useTitle } from '../../hooks';
+
+const Components = {
+  code({ node, inline, className, children, ...props }) {
+    const match = /language-(\w+)/.exec(className || '');
+    return !inline && match ? (
+      <SyntaxHighlighter
+        children={String(children).replace(/\n$/, '')}
+        style={dracula}
+        language={match[1]}
+        wrapLongLines={true}
+        PreTag='div'
+        {...props}
+      />
+    ) : (
+      <code className={className} {...props}>
+        {children}
+      </code>
+    );
+  },
+};
 
 const Blog = ({ params }) => {
+  const [location, setLocation] = useLocation();
+  const [blog, setBlog] = useState({});
+  useTitle('Blog ' + blog.title);
+
+  const fetchData = async () => {
+    let res = await fetch('http://localhost:8000/blog/' + params.id);
+    res = await res.json();
+    if (res['blog'] == null) {
+      setLocation('/blogs');
+    }
+    setBlog(res['blog']);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   if (isNaN(Number(params.id))) {
     return <Redirect to='/404' />;
   }
 
   return (
     <Container>
-      <h1>Welcome to Blog #{params.id}.</h1>
+      <Markdown
+        components={Components}
+        children={blog.content}
+        linkTarget='_blank'
+      />
     </Container>
   );
 };
@@ -17,10 +63,41 @@ const Blog = ({ params }) => {
 const Container = styled.div`
   color: ${p => p.theme.textColor};
   background-color: ${p => p.theme.backgroundColor};
-  height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
+  padding: 50px 250px 50px 250px;
+
+  @media only screen and (max-width: 600px) {
+    padding: 50px 10px 50px 10px;
+  }
+`;
+
+const Markdown = styled(ReactMarkdown)`
+  white-space: pre-wrap;
+
+  p {
+    font-size: 18px;
+  }
+
+  a {
+    color: ${p => p.theme.primaryColor};
+    text-decoration: none;
+  }
+
+  a:hover {
+    text-decoration: underline;
+  }
+
+  ul {
+    list-style-position: inside;
+  }
+
+  blockquote {
+    color: #666;
+    padding-left: 1.5em;
+    border-left: 4px solid ${p => p.theme.primaryColor};
+  }
 `;
 
 export default Blog;
--- src/pages/blogs/index.jsx
+++ src/pages/blogs/index.jsx
@@ -1,33 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import { BlogCard } from '../../components';
+import { useTitle } from '../../hooks';
 
-const Blogs = () => (
-  <Container>
-    <H1>Blogs!</H1>
-    <BlogsContainer>
-      {Array.from({ length: 10 }, (item, index) => (
-        <BlogCard
-          id={index}
-          title={index + ' Lorem ipsum dolor sit amet'}
-          timestamp='10 hrs ago'
-          summary='
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sagittis leo
-        at pulvinar accumsan. Proin consectetur ante eget ex vulputate, non tempus
-        nulla interdum. Integer nisl odio, ultrices fermentum erat gravida,
-        sodales placerat massa. Ut nec malesuada lorem. Cras accumsan luctus
-        faucibus. Curabitur suscipit turpis massa, nec egestas lectus mollis
-        blandit. Nulla gravida quis justo vitae vulputate. Integer quis neque
-        tempor, eleifend risus in, facilisis purus. Quisque ut orci lobortis,
-        egestas lorem vel, elementum augue. Proin vitae ligula auctor, sagittis
-        urna vel, cursus nunc. Praesent pulvinar eros eu faucibus mattis. Proin
-        sit amet pharetra ipsum. Aenean ullamcorper eros eget bibendum dictum. Sed'
-        />
-      ))}
-    </BlogsContainer>
-  </Container>
-);
+const Blogs = () => {
+  useTitle('Kulothungan - Blogs');
+  const [blogs, setBlogs] = useState([]);
+
+  const fetchData = async () => {
+    let res = await fetch('http://localhost:8000/blogs');
+    res = await res.json();
+    setBlogs(res['blogs']);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <Container>
+      <H1>Blogs!</H1>
+      <BlogsContainer>
+        {blogs.map(item => (
+          <BlogCard
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            timestamp='10 hrs ago'
+            summary={item.summary}
+          />
+        ))}
+      </BlogsContainer>
+    </Container>
+  );
+};
 
 const BlogsContainer = styled.div``;
 
@@ -35,7 +42,6 @@ const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
   color: ${p => p.theme.textColor};
   background-color: ${p => p.theme.backgroundColor};
   padding: 50px 250px 0 250px;
