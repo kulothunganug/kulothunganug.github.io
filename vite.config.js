69dd9ed475a9f41466df9b08e4320c7710fac801
Kulothungan16 <kulothunganug@gmail.com>
2021-11-05 18:32:17 +0530

    Added some babel config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      babel: {
+        compact: false,
+        minified: true,
+      },
+    }),
+  ],
   base: 'https://kulothungan16.github.io/',
-})
+});
